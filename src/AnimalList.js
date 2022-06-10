1e701b59b8ecc5d0ebca89a802f65c5303a9506b
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-10 14:45:59 -0700

    info showing up on page

---
+++ src/AnimalList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AnimalList({ animals }) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { animals } from './AnimalList';
+
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main animals={animals}/>
+      <Footer />
     </div>
   );
 }
